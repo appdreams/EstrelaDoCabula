403d4bc1346d9aeb1850a9975058a5d4e53c5443
appdreams <pvinicius@gmail.com>
2017-03-22 17:36:58 -0300

    Correção Upload Foto

--- app/src/main/java/br/com/appdreams/estreladocabula/activity/PrimeiroAcessoActivity.java
+++ app/src/main/java/br/com/appdreams/estreladocabula/activity/PrimeiroAcessoActivity.java
@@ -238,6 +238,8 @@ public class PrimeiroAcessoActivity extends BaseActivity
     {
         super.onActivityResult(requestCode, resultCode, data);
 
+
+
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null)
         {
             filePath = data.getData();
